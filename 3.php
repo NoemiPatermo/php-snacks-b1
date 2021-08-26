0a3fe252877a2fd99f0f39e7e21bce7f6849e795
noemi86-cpu <noemipatermo@gmail.com>
2021-08-26 16:48:05 +0200

    php-snacks

--- 3.php
+++ 3.php
@@ -6,4 +6,46 @@ Ogni array figlio avrà come chiave una data in questo formato:
 associati a quella data.
 Stampare ogni data con i relativi post.  
 */
+$posts = [
+
+    '10/01/2019' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '10/02/2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '15/05/2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 5'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 6'
+        ]
+    ],
+];
+//Stampare ogni data con i relativi post.
+//var_dump($posts);
+print_r($posts);
 ?>
