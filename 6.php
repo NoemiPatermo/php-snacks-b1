7dbe0aba43ec3fc26580d84c09454f03c4ef85b7
noemi86-cpu <noemipatermo@gmail.com>
2021-08-27 15:05:33 +0200

    php-snacks

--- 6.php
+++ 6.php
@@ -1,9 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <?php
 /*
 Utilizzare questo array https://pastebin.com/CkX3680A di partenza 
  e stampare tutti gli insegnanti in un rettangolo grigio e tutti i PM 
  in un rettangolo verde.
  */
+//stampa insegnanti in rettangolo grigio e pm in quello verde
 $db = [
     'teachers' => [
         [
@@ -26,6 +36,27 @@ $db = [
         ]
     ]
 ];
-//stampa insegnanti in rettangolo grigio e pm in quello verde
 
 ?>
+
+<div style="background-color: lightgray;">
+    <?php
+        for ($i=0; $i < count($db['teachers']); $i++) { 
+            echo "{$db['teachers'][$i]['name']} {$db['teachers'][$i]['lastname']} <br/>";
+        }
+    ?>
+
+</div>
+
+<div style="background-color: lightgreen;">
+    <?php
+        for ($x=0; $x < count($db['pm']); $x++) { 
+            echo "{$db['pm'][$x]['name']} {$db['pm'][$x]['lastname']} <br/>";
+        }
+    ?>
+
+</div>
+
+</body>
+</html>
+
